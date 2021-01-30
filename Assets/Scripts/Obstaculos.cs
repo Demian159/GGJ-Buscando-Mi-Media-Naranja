9b4f144027bcff1b8dc9ad13f872c109658dab06
German Dartsch <chocolateconceptual@gmail.com>
2021-01-29 23:11:00 -0300

    Cambios y a la cama

---
+++ Assets/Scripts/Obstaculos.cs
@@ -0,0 +1,16 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaculos : MonoBehaviour
+{
+    [SerializeField] private int danioACausar = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Jugador")
+        {
+            other.GetComponent<Vida>().ProcesarDanio(danioACausar);
+        }
+    }
+}*/
