276b403d717d6211da76739f3799d49592aae9eb
richardpine189 <73888797+richardpine189@users.noreply.github.com>
2021-01-31 11:40:25 -0300

    UI al 20%

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    [SerializeField] GameObject CreditsPanel;
+    [SerializeField] GameObject SettingPanel;
+
+    public void StartGame()
+    {        
+
+    }
+    public void Credits()
+    {
+
+    }
+    public void Setting()
+    {
+
+    }
+    public void ClosePanel(GameObject objToClose)
+    {
+        objToClose.SetActive(false);
+    }
+    
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
